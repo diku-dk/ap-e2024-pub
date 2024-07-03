f92e3102d02b662d6e2ea0724c4be265cba7bf30
Troels Henriksen <athas@sigkill.dk>
2024-07-03 21:33:50 +0200

    Pin Nixpkgs.

--- shell.nix
+++ shell.nix
@@ -1,6 +1,14 @@
-# Does not pin Nixpkgs. Do that on your own if you feel like it.
 let
-  pkgs = import <nixpkgs> {};
+  # Pinning nixpkgs for reproducibility. If you want to use your
+  # system Nixpkgs, use the other definition of 'nixpkgs' that is
+  # commented out below.
+  nixpkgs = builtins.fetchTarball {
+    url = https://github.com/NixOS/nixpkgs/archive/refs/tags/24.05.tar.gz;
+    sha256 = "sha256:1lr1h35prqkd1mkmzriwlpvxcb34kmhc9dnr48gkm8hh089hifmx";
+  };
+  # nixpkgs = <nixpkgs>;
+
+  pkgs = import nixpkgs {};
 in
 pkgs.stdenv.mkDerivation {
   name = "AP2024";
