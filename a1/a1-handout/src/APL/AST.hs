0a8739e338d5f628b9b728f19d264644d6499775
Simon Vinding Brodersen <simon@3450.dk>
2024-09-09 14:21:35 +0200

    Done Apply Lambda a1 assignment

---
+++ a1/a1-handout/src/APL/AST.hs
@@ -0,0 +1,28 @@
+module APL.AST (
+  VName,
+  Exp (..),
+  printExp,
+)
+where
+
+type VName = String
+
+data Exp
+  = CstInt Integer
+  | CstBool Bool
+  | Add Exp Exp
+  | Sub Exp Exp
+  | Mul Exp Exp
+  | Div Exp Exp
+  | Pow Exp Exp
+  | Eql Exp Exp
+  | If Exp Exp Exp
+  | Var VName
+  | Let VName Exp Exp
+  | Lambda VName Exp
+  | Apply Exp Exp
+  | TryCatch Exp Exp
+  deriving (Eq, Show)
+
+printExp :: Exp -> String
+printExp = undefined -- TODO
