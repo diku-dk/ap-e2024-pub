1316e9c2aee2117fafb939a683cf7435df6ee85c
Magnus Keinicke Parlo <magnus@skraentegaarden.dk>
2024-09-10 15:27:03 +0200

    E2 done

--- week2/handout/src/APL/Eval.hs
+++ week2/handout/src/APL/Eval.hs
@@ -28,10 +28,114 @@ envLookup v env = lookup v env
 
 type Error = String
 
-newtype EvalM a = EvalM a -- TODO
+newtype EvalM a = EvalM (Env -> Either Error a)
+
+instance Functor EvalM where
+  -- fmap :: (a -> b) -> EvalM a -> EvalM b
+  fmap = liftM
+
+instance Applicative EvalM where
+  -- pure :: a -> EvalM a
+  pure a = EvalM (\_ -> Right a)
+
+  -- (<*>) :: EvalM (a -> b) -> EvalM a -> EvalM b
+  (<*>) = ap
+
+instance Monad EvalM where
+  -- (>>=) :: EvalM a -> (a -> EvalM b) -> EvalM b
+  EvalM x >>= f =
+    EvalM
+      ( \env -> case x env of
+          Left err -> Left err
+          Right a -> let EvalM y = f a in y env
+      )
 
 runEval :: EvalM a -> Either Error a
-runEval = undefined -- TODO
+runEval (EvalM x) = x envEmpty
 
-eval :: Env -> Exp -> EvalM Val
-eval = undefined -- TODO
+failure :: Error -> EvalM a
+failure err = EvalM (\_ -> Left err)
+
+askEnv :: EvalM Env
+askEnv = EvalM (\env -> Right env)
+
+localEnv :: (Env -> Env) -> EvalM a -> EvalM a
+localEnv f (EvalM x) = EvalM (x . f)
+
+catch :: EvalM a -> EvalM a -> EvalM a
+catch (EvalM x) (EvalM y) =
+  EvalM
+    ( \env -> case x env of
+        Left _ -> y env
+        Right a -> Right a
+    )
+
+eval :: Exp -> EvalM Val
+eval (CstInt i) = return (ValInt i)
+eval (CstBool b) = return (ValBool b)
+eval (Add e1 e2) = do
+  v1 <- eval e1
+  v2 <- eval e2
+  case (v1, v2) of
+    (ValInt i1, ValInt i2) -> return (ValInt (i1 + i2))
+    _ -> failure "type error in addition"
+eval (Sub e1 e2) = do
+  v1 <- eval e1
+  v2 <- eval e2
+  case (v1, v2) of
+    (ValInt i1, ValInt i2) -> return (ValInt (i1 - i2))
+    _ -> failure "type error in subtraction"
+eval (Mul e1 e2) = do
+  v1 <- eval e1
+  v2 <- eval e2
+  case (v1, v2) of
+    (ValInt i1, ValInt i2) -> return (ValInt (i1 * i2))
+    _ -> failure "type error in multiplication"
+eval (Div e1 e2) = do
+  v1 <- eval e1
+  v2 <- eval e2
+  case (v1, v2) of
+    (ValInt i1, ValInt i2)
+      | i2 > 0 -> return (ValInt (i1 `div` i2))
+      | i2 == 0 -> failure "division by zero"
+      | i2 < 0 -> failure "division by negative number"
+    _ -> failure "type error in division"
+eval (Pow e1 e2) = do
+  v1 <- eval e1
+  v2 <- eval e2
+  case (v1, v2) of
+    (ValInt i1, ValInt i2)
+      | i2 >= 0 -> return (ValInt (i1 ^ i2))
+      | i2 < 0 -> failure "negative exponent"
+    _ -> failure "type error in exponentiation"
+eval (Eql e1 e2) = do
+  v1 <- eval e1
+  v2 <- eval e2
+  case (v1, v2) of
+    (ValInt i1, ValInt i2) -> return (ValBool (i1 == i2))
+    (ValBool b1, ValBool b2) -> return (ValBool (b1 == b2))
+    _ -> failure "type error in equality"
+eval (If e1 e2 e3) = do
+  v1 <- eval e1
+  case v1 of
+    ValBool True -> eval e2
+    ValBool False -> eval e3
+    _ -> failure "type error in if"
+eval (Var v) = do
+  env <- askEnv
+  case envLookup v env of
+    Just val -> return val
+    Nothing -> failure "unbound variable"
+eval (Let v e1 e2) = do
+  v1 <- eval e1
+  localEnv (envExtend v v1) (eval e2)
+eval (Lambda v e) = do
+  env <- askEnv
+  return (ValFun env v e)
+eval (Apply e1 e2) = do
+  v1 <- eval e1
+  v2 <- eval e2
+  case v1 of
+    ValFun env' v e -> localEnv (const (envExtend v v2 env')) (eval e)
+    _ -> failure "type error in application"
+eval (TryCatch e1 e2) = catch (eval e1) (eval e2)
--- week2/handout/src/APL/Eval_Tests.hs
+++ week2/handout/src/APL/Eval_Tests.hs
@@ -9,4 +9,56 @@ tests :: TestTree
 tests =
   testGroup
     "Evaluation"
-    []
+    [ testCase "CstInt" $
+        runEval (eval (CstInt 42)) @?= Right (ValInt 42),
+      testCase "CstBool" $
+        runEval (eval (CstBool True)) @?= Right (ValBool True),
+      testCase "Add" $
+        runEval (eval (Add (CstInt 1) (CstInt 2))) @?= Right (ValInt 3),
+      testCase "Sub" $
+        runEval (eval (Sub (CstInt 1) (CstInt 2))) @?= Right (ValInt (-1)),
+      testCase "Mul" $
+        runEval (eval (Mul (CstInt 2) (CstInt 3))) @?= Right (ValInt 6),
+      testCase "Div" $
+        runEval (eval (Div (CstInt 6) (CstInt 2))) @?= Right (ValInt 3),
+      testCase "Pow" $
+        runEval (eval (Pow (CstInt 2) (CstInt 3))) @?= Right (ValInt 8),
+      testCase "Eql" $
+        runEval (eval (Eql (CstInt 2) (CstInt 3))) @?= Right (ValBool False),
+      testCase "If" $
+        runEval (eval (If (CstBool True) (CstInt 1) (CstInt 2))) @?= Right (ValInt 1),
+      testCase "IfFalse" $
+        runEval (eval (If (CstBool False) (CstInt 1) (CstInt 2))) @?= Right (ValInt 2),
+      testCase "IfError" $
+        runEval (eval (If (CstInt 1) (CstInt 1) (CstInt 2))) @?= Left "type error in if",
+      testCase "AddError" $
+        runEval (eval (Add (CstInt 1) (CstBool True))) @?= Left "type error in addition",
+      testCase "SubError" $
+        runEval (eval (Sub (CstInt 1) (CstBool True))) @?= Left "type error in subtraction",
+      testCase "MulError" $
+        runEval (eval (Mul (CstInt 1) (CstBool True))) @?= Left "type error in multiplication",
+      testCase "DivError" $
+        runEval (eval (Div (CstInt 1) (CstBool True))) @?= Left "type error in division",
+      testCase "DivByZero" $
+        runEval (eval (Div (CstInt 1) (CstInt 0))) @?= Left "division by zero",
+      testCase "DivByNeg" $
+        runEval (eval (Div (CstInt 1) (CstInt (-1)))) @?= Left "division by negative number",
+      testCase "PowError" $
+        runEval (eval (Pow (CstInt 1) (CstBool True))) @?= Left "type error in exponentiation",
+      testCase "PowNeg" $
+        runEval (eval (Pow (CstInt 1) (CstInt (-1)))) @?= Left "negative exponent",
+      testCase "EqlError" $
+        runEval (eval (Eql (CstInt 1) (CstBool True))) @?= Left "type error in equality",
+      testCase "VarUnbound" $
+        runEval (eval (Var "x")) @?= Left "unbound variable",
+      testCase "Let" $
+        runEval (eval (Let "x" (CstInt 42) (Var "x"))) @?= Right (ValInt 42),
+      testCase "LetShadow" $
+        runEval (eval (Let "x" (CstInt 5) (Let "x" (CstInt 2) (Var "x")))) @?= Right (ValInt 2),
+      testCase "Lambda" $
+        runEval (eval (Apply (Lambda "x" (Var "x")) (CstInt 42))) @?= Right (ValInt 42),
+      testCase "Lambda2" $
+        runEval (eval (Apply (Lambda "x" (Lambda "y" (Add (Var "x") (Var "y"))) `Apply` CstInt 1) (CstInt 2))) @?= Right (ValInt 3),
+      testCase "TryCatch" $
+        runEval (eval (TryCatch (Div (CstInt 1) (CstInt 0)) (CstInt 42))) @?= Right (ValInt 42)
+    ]
