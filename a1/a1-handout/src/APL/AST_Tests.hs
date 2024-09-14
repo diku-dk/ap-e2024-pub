9f31d17d0910ff0ffd435f8bc4edd7837f09c7f0
Simon Vinding Brodersen <simon@3450.dk>
2024-09-14 09:02:24 +0200

    Move printExp to correct location

--- a1/a1-handout/src/APL/AST_Tests.hs
+++ a1/a1-handout/src/APL/AST_Tests.hs
@@ -1,6 +1,6 @@
 module APL.AST_Tests (tests) where
 
-import APL.AST (Exp (..))
+import APL.AST (Exp (..), printExp)
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (testCase, (@?=))
 
@@ -8,4 +8,59 @@ tests :: TestTree
 tests =
   testGroup
     "Prettyprinting"
-    []
+    [ testCase "printExp CstInt" $
+        printExp (CstInt 2)
+          @?= "2"
+    , --
+      testCase "printExp CstBool" $
+        printExp (CstBool True)
+          @?= "True"
+    , --
+      testCase "printExp Add" $
+        printExp (Add (CstInt 2) (CstInt 5))
+          @?= "(2 + 5)"
+    , --
+      testCase "printExp Sub" $
+        printExp (Sub (CstInt 10) (CstInt 5))
+          @?= "(10 - 5)"
+    , --
+      testCase "printExp Mul" $
+        printExp (Mul (CstInt 6) (CstInt 2))
+          @?= "(6 * 2)"
+    , --
+      testCase "printExp Div" $
+        printExp (Div (CstInt 3) (CstInt 6))
+          @?= "(3 / 6)"
+    , --
+      testCase "printExp Pow" $
+        printExp (Pow (CstInt 2) (CstInt 5))
+          @?= "(2 ** 5)"
+    , --
+      testCase "printExp Eql" $
+        printExp (Eql (CstInt 2) (CstInt 5))
+          @?= "(2 == 5)"
+    , --
+      testCase "printExp If" $
+        printExp (If (CstBool True) (CstInt 2) (CstInt 5))
+          @?= "if True then 2 else 5"
+    , --
+      testCase "printExp Var" $
+        printExp (Var "x")
+          @?= "x"
+    , --
+      testCase "printExp Let" $
+        printExp (Let "x" (CstInt 2) (Var "x"))
+          @?= "let x = 2 in x"
+    , --
+      testCase "printExp Lambda" $
+        printExp (Lambda "x" (Add (Var "x") (CstInt 5)))
+          @?= "\\x -> (x + 5)"
+    , --
+      testCase "printExp Apply" $
+        printExp (Apply (Var "f") (CstInt 5))
+          @?= "(f)(5)"
+    , --
+      testCase "printExp TryCatch" $
+        printExp (TryCatch (CstInt 0) (CstInt 1))
+          @?= "try 0 catch 1"
+    ]
--- a1/a1-handout/src/APL/Eval.hs
+++ a1/a1-handout/src/APL/Eval.hs
@@ -6,7 +6,6 @@ module APL.Eval (
   Env,
   envEmpty,
   eval,
-  printExp,
 )
 where
 
@@ -98,27 +97,3 @@ eval env (TryCatch e1 e2) =
   case eval env e1 of
     Left _ -> eval env e2
     Right e -> Right e
-
-printExp :: Exp -> String
-printExp e =
-  case e of
-    CstInt x -> show x
-    CstBool _ -> "True"
-    Add e1 e2 -> "(" ++ printExp e1 ++ " + " ++ printExp e2 ++ ")"
-    Sub e1 e2 -> "(" ++ printExp e1 ++ " - " ++ printExp e2 ++ ")"
-    Mul e1 e2 -> "(" ++ printExp e1 ++ " * " ++ printExp e2 ++ ")"
-    Div e1 e2 -> "(" ++ printExp e1 ++ " / " ++ printExp e2 ++ ")"
-    Pow e1 e2 -> "(" ++ printExp e1 ++ " ** " ++ printExp e2 ++ ")"
-    Eql e1 e2 -> "(" ++ printExp e1 ++ " == " ++ printExp e2 ++ ")"
-    If e1 e2 e3 ->
-      "if "
-        ++ printExp e1
-        ++ " then "
-        ++ printExp e2
-        ++ " else "
-        ++ printExp e3
-    Var var -> var
-    Let var e1 e2 -> "let " ++ var ++ " = " ++ printExp e1 ++ " in " ++ printExp e2
-    Lambda var e1 -> "\\" ++ var ++ " -> " ++ printExp e1
-    Apply e1 e2 -> "(" ++ printExp e1 ++ ")" ++ "(" ++ printExp e2 ++ ")"
-    TryCatch e1 e2 -> "try " ++ printExp e1 ++ " catch " ++ printExp e2
