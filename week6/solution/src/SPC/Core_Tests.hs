990166e065e41274a6e3c15b6047164957bab3af
Troels Henriksen <athas@sigkill.dk>
2024-09-16 15:52:22 +0200

    Draft solution of week 6 exercises.
    This reveals something about how we develop the course material.

---
+++ week6/solution/src/SPC/Core_Tests.hs
@@ -0,0 +1,41 @@
+module SPC.Core_Tests (tests) where
+
+import Control.Concurrent (threadDelay)
+import Data.IORef
+import SPC.Core
+import Test.Tasty (TestTree, localOption, mkTimeout, testGroup)
+import Test.Tasty.HUnit (testCase, (@?=))
+
+tests :: TestTree
+tests =
+  localOption (mkTimeout 3000000) $
+    testGroup
+      "SPC (core)"
+      [ testCase "simple-job" $ do
+          spc <- startSPC
+          ref <- newIORef False
+          j <- jobAdd spc $ Job (writeIORef ref True) 1
+          r1 <- jobStatus spc j
+          r1 @?= JobRunning
+          r2 <- jobWait spc j
+          r2 @?= Done
+          v <- readIORef ref
+          v @?= True,
+        testCase "timeout" $ do
+          spc <- startSPC
+          ref <- newIORef False
+          j <- jobAdd spc $ Job (threadDelay 2000000 >> writeIORef ref True) 1
+          r1 <- jobStatus spc j
+          r1 @?= JobRunning
+          r2 <- jobWait spc j
+          r2 @?= DoneTimeout,
+        testCase "cancel" $ do
+          spc <- startSPC
+          ref <- newIORef False
+          j <- jobAdd spc $ Job (threadDelay 20000000 >> writeIORef ref True) 1
+          r1 <- jobStatus spc j
+          r1 @?= JobRunning
+          jobCancel spc j
+          r2 <- jobWait spc j
+          r2 @?= DoneCancelled
+      ]
