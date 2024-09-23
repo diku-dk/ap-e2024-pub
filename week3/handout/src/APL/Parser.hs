603ce6f49b8f42fd4ba2f21ace64bbee3bc5fb52
Simon Vinding Brodersen <simon@3450.dk>
2024-09-23 09:33:05 +0200

    Work wee3 excercise

--- week3/handout/src/APL/Parser.hs
+++ week3/handout/src/APL/Parser.hs
@@ -4,30 +4,47 @@ import APL.AST (Exp (..), VName)
 import Control.Monad (void)
 import Data.Char (isAlpha, isAlphaNum, isDigit)
 import Data.Void (Void)
-import Text.Megaparsec
-  ( Parsec,
-    choice,
-    chunk,
-    eof,
-    errorBundlePretty,
-    many,
-    notFollowedBy,
-    parse,
-    parseTest,
-    satisfy,
-    some,
-    try,
-  )
+import Text.Megaparsec (
+  Parsec,
+  choice,
+  chunk,
+  eof,
+  errorBundlePretty,
+  many,
+  notFollowedBy,
+  parse,
+  parseTest,
+  satisfy,
+  some,
+  try,
+ )
 import Text.Megaparsec.Char (space)
 
 -- Do not change this definition.
 type Parser = Parsec Void String
 
 pExp :: Parser Exp
-pExp = undefined
+pExp =
+  choice
+    [ CstInt <$> lInteger
+    , Var <$> lVName
+    ]
 
 -- Do not change this definition.
 parseAPL :: FilePath -> String -> Either String Exp
 parseAPL fname s = case parse (space *> pExp <* eof) fname s of
   Left err -> Left $ errorBundlePretty err
   Right x -> Right x
+
+lexeme :: Parser a -> Parser a
+lexeme p = p <* space
+
+lInteger :: Parser Integer
+lInteger = lexeme $ read <$> some (satisfy isDigit) <* notFollowedBy (satisfy isAlpha)
+
+lVName :: Parser VName
+lVName = lexeme $
+  do
+    c <- satisfy isAlpha
+    cs <- many (satisfy isAlphaNum)
+    pure $ c : cs
